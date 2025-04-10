f0282ee2f82f06ec61b5d92b3c2b80fb2e02b954
KeshavxGupta <keshav40353@gmail.com>
2025-04-10 13:07:47 +0530

    first Commit

---
+++ FarmApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    city=models.CharField(max_length=100, blank=True)
+    state=models.CharField(max_length=100, blank=True)
+
+    def _str_(self):
+        return self.username
+    
---
+++ FarmApp/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+# Create your views here.
+
+def home_view(request):
+    return render(request,'home.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('cpassword')
+
+
+        if password != cpassword:
+            messages.error(request, 'Passwords do not match')
+
+        elif User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists')
+
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, 'Email already used')
+            
+        else:
+            try:
+                validate_password(password)
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                messages.success(
+                    request, 'Registration successful! You can now login.')
+                return redirect('login')
+            
+            except ValidationError as e:
+                for error in e:
+                    messages.error(request,error)
+
+    return render(request, 'signup.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+
+        user = authenticate(request, username=username, password=password)   #gives true or false
+        if user:
+                login(request, user)
+                return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'login.html')
